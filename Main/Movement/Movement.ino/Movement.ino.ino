fa1d40ba6ed55dee035f17ad08581674ed54f6cb
achen989 <90875105+achen989@users.noreply.github.com>
2023-02-16 10:56:35 -0500

    Update Movement.ino.ino

--- Main/Movement/Movement.ino/Movement.ino.ino
+++ Main/Movement/Movement.ino/Movement.ino.ino
@@ -12,44 +12,44 @@
 const int leftForward = 8;
 const int leftBackward = 9;
 const int rightForward = 10;
-const int rightBackward = 10;
-Sensor sense = new Sensor();
+const int rightBackward = 11;
+//Sensor sense = new Sensor();
 void setup()
 {
   pinMode(leftForward, OUTPUT);
   pinMode(leftBackward, OUTPUT);
   pinMode(rightForward, OUTPUT);
-  pinMODE(rightBackward, OUTPUT);
-  while (true) {
-    // Ensures bot is safe by moving away from white line boundary toward center of circle
-    avoidBoundaryLeft();
-    avoidBoundaryRight();
-    avoidBoundaryBack();
-    // Turns to get into attacking position when enemy bot is directly in front
-    // Chooses random direction to turn to make bot movement unpredictable
-    int rand = random()%2; // Generates 0 or 1     
-    while (!sense.inFront()) {
-      if (rand == 0) {
-        leftTurn();        
-      }
-      else {
-        rightTurn();
-      }
-    }
-    attack(); // Aims to knock enemy bot out of circle
-  }  
+  pinMode(rightBackward, OUTPUT);
 }
 
-void attack()
+void loop ()
 {
-  // Aims to knock enemy bot out of circle
-  // Charges
-  while (sense.inFront()) {
-    digitalWrite(leftForward, LOW);
-    digitalWrite(leftBackward, HIGH);
-    digitalWrite(rightForward, LOW);
-    digitalWrite(rightBackward, HIGH);
+  // Ensures bot is safe by moving away from white line boundary toward center of circle
+  /*avoidBoundaryLeft();
+  avoidBoundaryRight();
+  avoidBoundaryBack();
+  // Turns to get into attacking position when enemy bot is directly in front
+  // Chooses random direction to turn to make bot movement unpredictable
+  int rand = random()%2; // Generates 0 or 1     
+  while (!sense.inFront()) {
+    if (rand == 0) {
+      leftTurn();        
+    }
+    else {
+      rightTurn();
+    }
   }
+  attack(); // Aims to knock enemy bot out of circle*/
+  rightTurn();
+  delay(500);
+  leftTurn();
+  delay(500);
+  stop();
+  delay(1000);
+}
+
+void stop ()
+{
   // Brakes if enemy bot has dodged
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, LOW);
@@ -57,7 +57,25 @@ void attack()
   digitalWrite(rightBackward, LOW);
 }
 
-void avoidBoundaryLeft () {
+void attack()
+{
+  // Aims to knock enemy bot out of circle
+  // Charges
+  /*while (sense.inFront()) {*/
+  // For left motor LOW HIGH is forward
+  // For right motor HIGH LOW is forward
+  // Code to move the bot forwards
+  while (true)
+  {
+    digitalWrite(leftForward, LOW);
+    digitalWrite(leftBackward, HIGH);
+    digitalWrite(rightForward, HIGH);
+    digitalWrite(rightBackward, LOW);
+  }
+  stop();
+}
+
+/*void avoidBoundaryLeft () {
   // Helps the bot avoid danger by moving away when boundary on left
   while (sense.lineLeft()) {
     rightTurn90(); // Needs to be 90 degrees through trial and error to ensure we are moving towards inside of circle
@@ -79,57 +97,57 @@ void avoidBoundaryBack () {
     rightTurn180(); // Needs to be 180 degrees through trial and error to ensure we are moving towards inside of circle
     moveForward(); // Moves to center of circle to avoid boundary
   }
-}
+}*/
 
-void moveForward () {
+/*void moveForward () {
   // Moves bot forward slightly to avoid boundary
   // Bot moves forward for 1 second
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, HIGH);
-  digitalWrite(rightForward, LOW);
-  digitalWrite(rightBackward, HIGH);
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
   delay (1000);
   // Brakes the bot
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, LOW);
-}
+}*/
 
-void leftTurn ()
-{
-  // Turns the bot right for defense and to get into attacking position by making one wheel spin faster
-  digitalWrite(leftForward, LOW);
-  digitalWrite(leftBackward, HIGH);
-  digitalWrite(rightForward, HIGH);
-  digitalWrite(rightBackward, LOW);
-}
-
-void rightTurn()
+void rightTurn ()
 {
   // Turns the bot right for defense and to get into attacking position by making one wheel spin faster
   digitalWrite(leftForward, HIGH);
   digitalWrite(leftBackward, LOW);
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+}
+
+void leftTurn()
+{
+  // Turns the bot right for defense and to get into attacking position by making one wheel spin faster
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, HIGH);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, HIGH);
+}
+
+/*void rightTurn90 () {
+  // Turns the bot left 90 degrees
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, LOW);
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+  delay(); // x to be determined upon testing rotation speed
+  stop ();
 }
 
 void leftTurn90 () {
-  // Turns the bot left 90 degrees
+  // Turns the bot right 90 degrees
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, HIGH);
-  digitalWrite(rightForward, HIGH);
-  digitalWrite(rightBackward, LOW);
-  delay(/*x*/); // x to be determined upon testing rotation speed
-  stop ();
-}
-
-void rightTurn90 () {
-  // Turns the bot right 90 degrees
-  digitalWrite(leftForward, HIGH);
-  digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, HIGH);
-  delay(/*x*/); // x to be determined upon testing rotation speed
+  delay(); // x to be determined upon testing rotation speed
   stop ();
-}
+}*/
