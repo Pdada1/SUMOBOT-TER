5132d812d281c1ab76b21182e2b5066434293428
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2023-02-16 11:09:02 -0500

    Update Sensor.ino
    Added overlapping detection.
    Single detection still in the works but almost complete.
    Sensing coming along overall

--- Sensing/Sensing.ino
+++ Sensing/Sensing.ino
@@ -1,40 +1,45 @@
-#include "FastTrig.h"
+
+class Ultrasonic {
+  int trigger_pin, echo_pin;  //pins on which the pulse is sent and recieved
+public:
+  Ultrasonic() {
+  }
+
+  Ultrasonic(int trigger_pin, int echo_pin)
+    : trigger_pin(trigger_pin), echo_pin(echo_pin) {  //ultrasonic constructor
+    pinMode(trigger_pin, OUTPUT);
+    pinMode(echo_pin, INPUT);
+  }
+  //if an object is detected, true is returned if the object is within the threshold, else false is returned.
+  long object_detected() {
+    static const unsigned long PULSE_MIN = 7000, PULSE_MAX = 9400;  // duration of pulse in microseconds
+    digitalWrite(trigger_pin, HIGH);
+    delayMicroseconds(15);
+    digitalWrite(trigger_pin, LOW);
+    unsigned long pulse = pulseIn(echo_pin, HIGH);
+    return microsecondsToCentimeters(pulse);
+  }
+  long microsecondsToCentimeters(long microseconds) {
+    return microseconds / 29 / 2;
+  }
+};
 Ultrasonic sensor1;
 Ultrasonic sensor2;
-class Ultrasonic {
-    int trigger_pin, echo_pin;//pins on which the pulse is sent and recieved
-  public:
-    Ultrasonic(int trigger_pin, int echo_pin): trigger_pin(trigger_pin), echo_pin(echo_pin) {//ultrasonic constructor
-      pinMode(trigger_pin, OUTPUT);
-      pinMode(echo_pin, INPUT);
-    }
-    //if an object is detected, true is returned if the object is within the threshold, else false is returned.
-    long object_detected() {
-      static const unsigned long PULSE_MIN = 7000, PULSE_MAX = 9400; // duration of pulse in microseconds
-      digitalWrite(trigger_pin, HIGH);
-      delayMicroseconds(15);
-      digitalWrite(trigger_pin, LOW);
-      unsigned long pulse = pulseIn(echo_pin, HIGH);
-      return microsecondsToCentimeters(pulse);
-    }
-    long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
+double dcalc(long dsense, long d2, long d3) {
+  double s = (dsense + d2 + d3) / 2;
+  double d = 2 * sqrt((double)(s * (s - dsense) * (s - d2) * (s - d3))) / (double)(dsense);
+  return d;
 }
-};
 
-long dcalc(long dsense, long d1, long d2)
-{
-   double angle1 = icos(double(pow(d2,2)-pow(d1,2)-pow(dsense,2))/(2*d1*dsense));
-   double angle2 = icos(double(pow(d1,2)-pow(d2,2)-pow(dsense,2))/(2*d2*dsense));
-
-}
 void setup() {
-Ultrasonic sensor1 = Ultrasonic(1, 2);
-Ultrasonic sensor2 = Ultrasonic(3, 4);
-long sensord=20; //to change for later to cm
+  sensor1 = Ultrasonic(2, 3);
+  sensor2 = Ultrasonic(4, 5);
+  Serial.begin(9600);
 }
 
 void loop() {
-  unsigned long d1= sensor1.object_detected();
-  unsigned long d2= sensor1.object_detected();
-  
+  unsigned long d2 = sensor1.object_detected();
+  unsigned long d3 = sensor2.object_detected();
+  long snsed = 7;  //to change for later to cm
+  Serial.println(dcalc(snsed, d2, d3));
+}
