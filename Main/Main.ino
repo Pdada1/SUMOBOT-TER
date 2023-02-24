70808d8138e217d207dcc0a4a6c7a041c5404090
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2023-02-24 11:02:35 -0500

    Commit

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,11 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
- printf("Something");
- printf("Something was changed");
+int sensorValue = 0;
+void setup()
+{
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
+  pinMode(A2, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-    printf("Something");
-    printf("Change #2");
+void loop()
+{
+  // read the value from the sensor
+  sensorValue = analogRead(A0);
+  // print the sensor reading so you know its range
+  Serial.println(sensorValue);
+  Serial.println(analogRead(A1));
+  Serial.println(analogRead(A2));
+ // map the sensor reading to a range for the LED
+  delay(100); // Wait for 100 millisecond(s)
 }
